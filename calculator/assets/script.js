bc2aa2ccbd7b358d25d98ec816c34b10afc121db
Matt Ellicott <mattellicott@gmail.com>
2023-10-19 12:47:25 -0500

    Add Decimal support

--- calculator/assets/script.js
+++ calculator/assets/script.js
@@ -1,14 +1,14 @@
-const DEFAULT_VALUE = 0;
+const DEFAULT_VALUE = "0";
 
-const totalScreen = document.getElementById("totalScreen");
-const inputScreen = document.getElementById("inputScreen");
+const historyScreen = document.getElementById("historyScreen").firstChild;
+const inputScreen = document.getElementById("inputScreen").firstChild;
 const clearBtn = document.getElementById("clearBtn");
 const deleteBtn = document.getElementById("deleteBtn");
+const decimalBtn = document.getElementById("decimalBtn");
 const divideBtn = document.getElementById("divideBtn");
 const multiplyBtn = document.getElementById("multiplyBtn");
 const subtractBtn = document.getElementById("subtractBtn");
 const addBtn = document.getElementById("addBtn");
-const decimalBtn = document.getElementById("decimalBtn");
 const zeroBtn = document.getElementById("zeroBtn");
 const oneBtn = document.getElementById("oneBtn");
 const twoBtn = document.getElementById("twoBtn");
@@ -19,3 +19,112 @@ const sixBtn = document.getElementById("sixBtn");
 const sevenBtn = document.getElementById("sevenBtn");
 const eightBtn = document.getElementById("eightBtn");
 const nineBtn = document.getElementById("nineBtn");
+
+clearBtn.onclick = () => evalClear();
+deleteBtn.onclick = () => evalDelete();
+decimalBtn.onclick = () => evalDecimal();
+equalBtn.onclick = () => evalEqual();
+divideBtn.onclick = () => evalOperator("÷");
+multiplyBtn.onclick = () => evalOperator("×");
+subtractBtn.onclick = () => evalOperator("−");
+addBtn.onclick = () => evalOperator("+");
+zeroBtn.onclick = () => evalNumber(0);
+oneBtn.onclick = () => evalNumber(1);
+twoBtn.onclick = () => evalNumber(2);
+threeBtn.onclick = () => evalNumber(3);
+fourBtn.onclick = () => evalNumber(4);
+fiveBtn.onclick = () => evalNumber(5);
+sixBtn.onclick = () => evalNumber(6);
+sevenBtn.onclick = () => evalNumber(7);
+eightBtn.onclick = () => evalNumber(8);
+nineBtn.onclick = () => evalNumber(9);
+
+window.onload = () => evalClear();
+
+let operand = "";
+let lastOperator = "";
+let operandList = [];
+let errorLogged = false;
+
+function evalNumber(num) {
+  if (errorLogged) return;
+
+  operand += num;
+  updateScreens();
+}
+
+function evalOperator(newOperator) {
+  if (errorLogged) return;
+
+  if (operand) appendOperandToList();
+  if (operandList.length == 2) {
+    operandList = [evalResult(operandList, lastOperator)];
+  }
+
+  lastOperator = newOperator;
+  updateScreens();
+}
+
+function appendOperandToList() {
+  operandList.push(Number(operand));
+  clearOperand();
+}
+
+function evalEqual() {
+  if (operand) appendOperandToList();
+  if (operandList.length != 2 || !lastOperator || errorLogged) return;
+
+  operandList = [evalResult(operandList, lastOperator)];
+  lastOperator = "";
+  updateScreens();
+}
+
+function evalClear() {
+  errorLogged = false;
+  lastOperator = "";
+  operandList = [];
+  clearOperand();
+  updateScreens();
+  inputScreen.textContent = DEFAULT_VALUE;
+}
+
+function evalDelete() {
+  if (errorLogged) return;
+
+  operand = operand.slice(0, -1);
+  updateScreens();
+}
+
+function evalDecimal() {
+  evalNumber(".");
+}
+
+function evalResult([leftOperand, rightOperand], lastOperator) {
+  switch (lastOperator) {
+    case "÷":
+      if (rightOperand === 0) {
+        logError("DIV BY 0 ERROR");
+      } else return leftOperand / rightOperand;
+    case "×":
+      return leftOperand * rightOperand;
+    case "−":
+      return leftOperand - rightOperand;
+    case "+":
+      return leftOperand + rightOperand;
+  }
+}
+
+function clearOperand() {
+  operand = "";
+}
+
+function logError(error) {
+  errorLogged = error;
+}
+
+function updateScreens() {
+  historyScreen.textContent = errorLogged
+    ? errorLogged
+    : operandList.join("") + lastOperator;
+  inputScreen.textContent = errorLogged ? errorLogged : operand;
+}
