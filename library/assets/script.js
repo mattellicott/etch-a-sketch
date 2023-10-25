21f4bdb0f309b5f54bafd2398ea48e1792f9c0fc
Matt Ellicott <mattellicott@gmail.com>
2023-10-25 13:22:40 -0500

    Refactor Books object constructor to Book class All remaining variables, functions, and handlers to Library class

--- library/assets/script.js
+++ library/assets/script.js
@@ -1,84 +1,98 @@
-"use strict";
+class Library {
+  addBookBtn = document.getElementById("addBookBtn");
+  bookDialog = document.getElementById("bookDialog");
+  bookForm = document.getElementById("bookForm");
+  bookGrid = document.getElementById("bookGrid");
 
-const addBookBtn = document.getElementById("addBookBtn");
-const bookDialog = document.getElementById("bookDialog");
-const bookForm = document.getElementById("bookForm");
-const bookGrid = document.getElementById("bookGrid");
-
-let myLibrary = [];
-
-addBookBtn.onclick = () => bookDialog.showModal();
-bookForm.onsubmit = (e) => {
-  e.preventDefault();
-  myLibrary.push(getBookFormData());
-  bookGrid.appendChild(createCard(myLibrary.at(-1)));
-  bookForm.reset();
-  bookDialog.close();
-};
-
-function Book(title, author, pages, hasRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.hasRead = hasRead;
-}
-
-function getBookFormData() {
-  const title = document.getElementById("titleInput").value;
-  const author = document.getElementById("authorInput").value;
-  const pages = document.getElementById("pagesInput").value;
-  const hasRead = document.getElementById("hasReadInput").checked;
-
-  return new Book(title, author, pages, hasRead);
-}
-
-function createCard(book) {
-  const card = document.createElement("div");
-  card.classList.add("bookCard");
-
-  for (let [type, value] of Object.entries(book)) {
-    if (type === "hasRead") card.appendChild(createCardButtons(value));
-    else card.appendChild(createCardP(type, value));
+  constructor() {
+    this.books = [];
   }
 
-  return card;
+  addBookBtnHandler() {
+    this.addBookBtn.onclick = () => bookDialog.showModal();
+  }
+
+  bookFormHandler() {
+    this.bookForm.onsubmit = (e) => {
+      e.preventDefault();
+      this.getBookFormData();
+      this.bookGrid.appendChild(this.createCardFromLatestBook());
+      this.bookForm.reset();
+      this.bookDialog.close();
+    };
+  }
+
+  getBookFormData() {
+    const title = document.getElementById("titleInput").value;
+    const author = document.getElementById("authorInput").value;
+    const pages = document.getElementById("pagesInput").value;
+    const hasRead = document.getElementById("hasReadInput").checked;
+
+    this.books.push(new Book(title, author, pages, hasRead));
+  }
+
+  createCardFromLatestBook() {
+    const card = document.createElement("div");
+    card.classList.add("bookCard");
+
+    for (let [type, value] of Object.entries(this.books.at(-1))) {
+      if (type === "hasRead") card.appendChild(this.createCardButtons(value));
+      else card.appendChild(this.createCardP(type, value));
+    }
+
+    return card;
+  }
+
+  createCardP(type, value) {
+    const pElement = document.createElement("p");
+    pElement.classList.add(type);
+    pElement.textContent = type === "pages" ? `${value} pages` : value;
+
+    return pElement;
+  }
+
+  createCardButtons(hasRead) {
+    const cardButtons = document.createElement("div");
+    cardButtons.classList.add("cardBtns");
+
+    cardButtons.appendChild(this.createReadBtn(hasRead));
+    cardButtons.appendChild(this.createRemoveBtn());
+
+    return cardButtons;
+  }
+
+  createReadBtn(hasRead) {
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("readBtn");
+    readBtn.addEventListener("click", () => readBtn.classList.toggle("read"));
+
+    if (hasRead) readBtn.classList.add("read");
+    readBtn.textContent = "Read";
+
+    return readBtn;
+  }
+
+  createRemoveBtn() {
+    const removeBtn = document.createElement("button");
+    removeBtn.addEventListener("click", (e) =>
+      e.currentTarget.parentNode.parentNode.remove(),
+    );
+    removeBtn.textContent = "Remove";
+
+    return removeBtn;
+  }
 }
 
-function createCardP(type, value) {
-  const pElement = document.createElement("p");
-  pElement.classList.add(type);
-  pElement.textContent = type === "pages" ? `${value} pages` : value;
-
-  return pElement;
+class Book {
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+  }
 }
 
-function createCardButtons(hasRead) {
-  const cardButtons = document.createElement("div");
-  cardButtons.classList.add("cardBtns");
+const myLibrary = new Library();
 
-  cardButtons.appendChild(createReadBtn(hasRead));
-  cardButtons.appendChild(createRemoveBtn());
-
-  return cardButtons;
-}
-
-function createReadBtn(hasRead) {
-  const readBtn = document.createElement("button");
-  readBtn.classList.add("readBtn");
-  readBtn.addEventListener("click", () => readBtn.classList.toggle("read"));
-
-  if (hasRead) readBtn.classList.add("read");
-  readBtn.textContent = "Read";
-
-  return readBtn;
-}
-
-function createRemoveBtn() {
-  const removeBtn = document.createElement("button");
-  removeBtn.addEventListener("click", (e) =>
-    e.currentTarget.parentNode.parentNode.remove(),
-  );
-  removeBtn.textContent = "Remove";
-
-  return removeBtn;
-}
+myLibrary.addBookBtnHandler();
+myLibrary.bookFormHandler();
